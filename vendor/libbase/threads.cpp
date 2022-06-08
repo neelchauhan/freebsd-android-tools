32084196913557ee0171930035828a88b59fce6b
Neel Chauhan <neel@neelc.org>
2022-06-08 09:58:48 -0700

    Fix crash with real path and pthread ID

--- vendor/libbase/threads.cpp
+++ vendor/libbase/threads.cpp
@@ -21,6 +21,8 @@
 
 #if defined(__APPLE__)
 #include <pthread.h>
+#elif defined(__FreeBSD__)
+#include <pthread_np.h>
 #elif defined(__linux__) && !defined(__ANDROID__)
 #include <syscall.h>
 #elif defined(_WIN32)
@@ -37,6 +39,8 @@ uint64_t GetThreadId() {
   uint64_t tid;
   pthread_threadid_np(NULL, &tid);
   return tid;
+#elif defined(__FreeBSD__)
+  return pthread_getthreadid_np();
 #elif defined(__linux__)
   return syscall(__NR_gettid);
 #elif defined(_WIN32)
